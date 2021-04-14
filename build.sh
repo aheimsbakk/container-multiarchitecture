ce44f28c025bd124e063f67888086dd052c8167f
aheimsbakk <arnulf.heimsbakk@gmail.com>
2021-04-14 18:24:12 +0200

    plattform

--- build.sh
+++ build.sh
@@ -70,10 +70,11 @@ do
   echo
 
   dockerfile=$(get_dockerfile "$arch" "$DOCKERFILE_PATH")
-  #plattform="$(echo "$arch" | sed -E 's#(.*)(v.*)#\1/\2#g' | sed -E 's#(.*)#linux/\1#')"
+  plattform="$(echo "$arch" | sed -E 's#(.*)(v.*)#\1/\2#g' | sed -E 's#(.*)#linux/\1#')"
+  echo "$plattform"
   cd "$(dirname "$DOCKERFILE_PATH")" || return
   echo "$dockerfile" |
-    $DOCKER_CMD build --tag "$IMAGE_NAME-$arch" --arch "$arch" -
+    $DOCKER_CMD build --tag "$IMAGE_NAME-$arch" --plattform "$plattform" -
     #$DOCKER_CMD build --tag "$IMAGE_NAME-$arch" -
 done
 
